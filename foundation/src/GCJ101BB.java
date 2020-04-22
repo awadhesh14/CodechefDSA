31220de670a818dd54a81c718ab5ec63c3762c9e
awadhesh14 <dapst8@gmail.com>
2020-04-22 23:22:13 +0530

    first greedy question

--- foundation/src/GCJ101BB.java
+++ foundation/src/GCJ101BB.java
@@ -1,5 +1,6 @@
-
+/*Template for every code*/
 import java.io.*;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 class Main {
@@ -59,10 +60,31 @@ class Main {
         }
     }
     public static void main(String[] args) throws Exception {
-        FastReader sc = new FastReader(new FileInputStream("src/input.in"));
+        FastReader sc = new FastReader(System.in);
         int t = sc.nextInt();
-        while (t-- > 0) {
-            System.out.println(sc.nextInt()  + "A");
+        int N,K,T;
+        long B;
+        for(int _t=1;_t<=t;_t++){
+            N=sc.nextInt();K=sc.nextInt();B=sc.nextLong();T=sc.nextInt();
+            ArrayList<Integer> X = new ArrayList<>(N);
+            for(int i=0;i<N;i++)
+                X.add(sc.nextInt());
+            ArrayList<Integer> V = new ArrayList<>(N);
+            for(int i=0;i<N;i++)
+                V.add(sc.nextInt());
+            int k=0,jumps=0,stragglers=0;
+            for (int i=N-1;i>=0;i--){
+                if ((float)(B-X.get(i))/V.get(i) <= T) {
+                    k++;
+                    jumps += stragglers;
+                }
+                else stragglers++;
+                if (k>=K) break;
+            }
+            if (k>=K)
+                System.out.println("Case #"+_t+": "+jumps );
+            else
+                System.out.println("Case #"+_t+": "+"IMPOSSIBLE");
         }
     }
 }
