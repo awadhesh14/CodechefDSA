7f28116f4dfac3d5f61d91ef00fd72a89e665dea
awadhesh14 <dapst8@gmail.com>
2020-04-23 16:14:00 +0530

    KNPSK Knapsack Problem codechef(medium)

--- foundation/src/KNPSK.java
+++ foundation/src/KNPSK.java
@@ -1,6 +1,6 @@
 
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
 
 class Main {
     static class FastReader
@@ -59,10 +59,93 @@ class Main {
         }
     }
     public static void main(String[] args) throws Exception {
-        FastReader sc = new FastReader(new FileInputStream("src/input.in"));
-        int t = sc.nextInt();
+        FastReader sc = new FastReader(System.in);
+        int t = sc.nextInt(),w,c,W = 0;
+        boolean single;
+        ArrayList<Integer> arr = new ArrayList<>(t),brr = new ArrayList<>(t);
+        Stack<Integer> a=new Stack<>(),b = new Stack<>();
         while (t-- > 0) {
-            System.out.println(sc.nextInt()  + "A");
+            w=sc.nextInt();c=sc.nextInt();
+            if (w==1) arr.add(c);
+            else  brr.add(c);
+            W += w;
         }
+        long[] res = new long[W+1];
+        arr.sort(Comparator.comparingInt(p -> p));
+        brr.sort(Comparator.comparingInt(p -> p));
+        /*brr.forEach(s -> System.out.println(s+ " "));*/
+        a.addAll(arr);b.addAll(brr);
+        /*even*/
+        int costa,costb;
+        long ans=0;
+        for(int i=2;i<=W;i+=2){
+            costa=0;costb=0;
+            single=false;
+            if(a.size()>=2){
+                costa = a.peek()+a.elementAt(a.size()-2);
+            }
+            else if(a.size()==1){
+                costa = a.peek();
+                single = true;
+            }
+            if(!b.empty()){
+                costb = b.peek();
+            }
+            if(costa>costb){
+                if(single){
+                    a.pop();
+                }
+                else {
+                    a.pop();a.pop();
+                }
+                ans += costa;
+            }
+            else{
+                b.pop();
+                ans+=costb;
+            }
+            res[i] = ans;
+        }
+        /*odd*/
+        a.addAll(arr);b.addAll(brr);
+        ans=0;
+        if(!a.empty()){
+            ans=a.peek();
+            res[1]=a.pop();
+        }
+        else{
+            res[1]=0;
+        }
+        for(int i=3;i<=W;i+=2){
+            costa=0;costb=0;
+            single=false;
+            if(a.size()>=2){
+                costa = a.peek()+a.elementAt(a.size()-2);
+            }
+            else if(a.size()==1){
+                costa = a.peek();
+                single = true;
+            }
+            if(!b.empty()){
+                costb = b.peek();
+            }
+            if(costa>costb){
+                if(single){
+                    a.pop();
+                }
+                else {
+                    a.pop();a.pop();
+                }
+                ans += costa;
+            }
+            else{
+                b.pop();
+                ans+=costb;
+            }
+            res[i] = ans;
+        }
+        for(int i=1;i<=W;i++)
+            System.out.print(res[i]+" ");
     }
+
 }
